064742126a74c52c316cad7a56bcb1886c2a1bd7
Aleks24dan <aleks24dan@gmail.com>
2021-09-25 18:10:04 +0200

    23534

---
+++ src/components/ContactForm/ContactForm.styled.js
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled";
+
+export const form = styled.form`
+flex-direction: column;
+display: flex;
+justify-content: center;
+text-align:centr;
+`
+
+export const LabelInput = styled.label`
+font-weight: 700;
+font-size: 17px;
+margin:0 auto;
+flex-direction: column;
+display: flex;
+width: 300px;
+justify-content: center;
+text-align:centr;
+`
+
+export const Input = styled.input`
+margin-right: ;
+padding: 5px;
+border-radius: 5px;
+box-shadow: 1px 2px 3px rgb(85, 42, 85);
+`
+
+export const Button = styled.button`
+padding: 5px;
+border-radius: 5px;
+box-shadow: 3px 2px 3px rgb(85, 42, 85);
+font-size: 14px;
+font-weight:700;
+margin-top: 10px;
+  
+`
+export const Label = styled.label`
+display:flex;
+flexdiraction:column;
+justify-content: center;
+text-align:centr;
+`  
